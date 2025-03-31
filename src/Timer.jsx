9d43bb58e305d33cc6ddd219599db0b3ba243d27
soumya1925 <113868085+soumya1925@users.noreply.github.com>
2025-03-31 16:06:31 +0530

    Initial commit

---
+++ src/Timer.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useRef } from "react";
+import "./App.css";
+
+const Timer = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const timerRef = useRef(null);
+
+  const startTimer = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+  };
+
+  const stopTimer = () => {
+    if (isRunning) {
+      clearInterval(timerRef.current);
+      setIsRunning(false);
+    }
+  };
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    setIsRunning(false);
+    setTime(0);
+  };
+
+ 
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${minutes}:${secs.toString().padStart(2, "0")}`;
+  };
+
+  return (
+    <div className="main-container">
+      <h1>Stopwatch</h1>
+      <p>Time: {formatTime(time)}</p>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={resetTimer}>Reset</button>
+    </div>
+  );
+};
+
+export default Timer;
